462c87d400a5a230c802d7b6f0a5e4bfcc5ad1fe
Twack14 <taylor_womack@yahoo.com>
2022-03-29 00:59:25 -0500

    update

--- commands/willsmith.js
+++ commands/willsmith.js
@@ -4,7 +4,7 @@ var gif = 'https://c.tenor.com/BpCSsMEt-54AAAAd/stfu-shut-up.gif'
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('willsmith')
-		.setDescription('Replies with a gif of Will Smith'),
+		.setDescription('Replies with a gif of Will Smith slapping Chris Rock'),
 	async execute(interaction) {
         await interaction.reply(`Keep my wife's name out yo FUCKIN MOUTH\n${gif}`);
 	},
